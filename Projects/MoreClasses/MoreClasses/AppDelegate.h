de13ffe45596cb25f6e948eb656d1da24dc751b9
Jaykong <kongyunpeng2011@sina.com>
2015-11-26 16:16:38 +0800

    第二节课后

---
+++ Projects/MoreClasses/MoreClasses/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoreClasses
+//
+//  Created by trainer on 11/25/15.
+//  Copyright © 2015 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

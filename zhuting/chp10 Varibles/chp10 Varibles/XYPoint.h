5126cd3ed37d9c8389324ff09ab6e688030cd751
wenslow <wenslow@vip.qq.com>
2015-12-03 20:40:55 +0800

    Homework of ZT 1202

---
+++ zhuting/chp10 Varibles/chp10 Varibles/XYPoint.h
@@ -0,0 +1,17 @@
+//
+//  XYPoint.h
+//  chp8 inherit
+//
+//  Created by Wenslow on 15/11/26.
+//  Copyright © 2015年 Wenslow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYPoint : NSObject
+
+@property double x, y;
+
+- (void)setX:(double)xVal andY: (double)yVal;
+
+@end
